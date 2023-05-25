8f43933677a96fbcd30d029c7523aff7df8a5f0a
bharathchowdary <Saibharath0459@gmail.com>
2023-05-24 19:53:04 -0500

    Initial commit

---
+++ src/main/java/com/sony/MethodExamples.java
@@ -0,0 +1,22 @@
+package com.sony;
+
+public class MethodExamples {
+    int num = 13;
+
+    public static void main(String[] args) {
+
+    }
+
+
+        public static boolean isEven ( int num){
+
+            if (num % 2 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+
+        }
+
+}
